31c8dda300a2174cd5ac1685c7bae95385a6ed5b
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-06-16 23:00:46 +0300

    add content

---
+++ info-website/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "../images/phoenix.png";
+
+const navbar = ()=>{
+    return(
+        <div className="App">
+            <div className="leftSide"> 
+                <img  src="{Logo}"/>
+            </div>
+            <div className="rightSide"> </div>
+
+        </div>
+    )
+};
+
+export default navbar;
